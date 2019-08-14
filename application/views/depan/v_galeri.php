ec4c99ecae0d644b168bf26d5d0cb81f743754a5
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2019-08-14 13:52:21 +0800

    sedikit perubahan

--- application/views/depan/v_galeri.php
+++ application/views/depan/v_galeri.php
@@ -158,65 +158,28 @@
 
 					<div class="row">
 						<div class="col-md-12">
-
 							<ul class="nav nav-pills sort-source" data-sort-id="portfolio" data-option-key="filter">
-								<li data-option-value="*" class="active"><a href="#">Lihat Semua</a></li>
-								<?php
-									foreach ($alb->result_array() as $i) {
-									$alb_id=$i['album_id'];
-									$alb_nama=$i['album_nama'];
-
-								?>
-								<li data-option-value=".<?php echo $alb_id?>"><a href="#"><?php echo $alb_nama;?></a></li>
-							<?php }?>
-								<li data-option-value=".instagram"><a href="#">Instagram</a></li>
 							</ul>
-
-							<hr>
-
 							<div class="row">
-
+							
 								<div class="sort-destination-loader sort-destination-loader-showing">
 									<ul class="image-gallery sort-destination lightbox" data-sort-id="portfolio" data-plugin-options="{'delegate': 'a.lightbox-portfolio', 'type': 'image', 'gallery': {'enabled': true}}">
-										<?php
-                                          foreach ($all_galeri->result_array() as $a) {
-                                            $id=$a['galeri_album_id'];
-                                            $judul=$a['galeri_judul'];
-                    						$caption=$a['galeri_caption'];
-                                            $gambar=$a['galeri_gambar'];
-                    
-                                        ?>
-										<li class="col-md-3 col-sm-6 col-xs-12 isotope-item <?php echo $id;?>">
-											<div class="image-gallery-item">
-													<div class="image-gallery-item">
-														<span class="thumb-info thumb-info-hide-wrapper-bg">
-														<span class="thumb-info-wrapper">
-															<a href="<?php echo base_url().'template/galeri/'.$gambar?>" class="lightbox-portfolio">
-															<img src="<?php echo base_url().'template/galeri/'.$gambar?>" class="img-responsive" alt="">
-															</a>
-															<span class="thumb-info-title">
-																<span class="thumb-info-inner"><?php echo $judul;?></span>
-															</span>
-														</span>
-														<span class="thumb-info-caption">
-															<span class="thumb-info-caption-text"><?php echo $caption;?></span>
-														</span>
-													</span>
-													</div>
-											</div>
-										</li>
-									<?php }?>
+										
 									</ul>
+									
 									<h2><center>Our Gallery From Instagram</center></h2>
 									<div class="row">
 										<div id="instafeed">
+										
 										</div>
 									</div>
 								</div>
 							</div>
+							
 						</div>
 					</div>
-					
+					<hr class="tall">
+					<center><a href="https://www.instagram.com/bosowaschoolmks/" target="_blank" class="btn btn-primary">Click Here for More Photos</a></center>
 				</div>
 				
 			<?php $this->load->view('depan/v_footer');?>
--- template/js/instagram/custom.js
+++ template/js/instagram/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         target: 'instafeed',
         sortBy: 'most-recent',
 		resolution: 'low_resolution',
-		template: '<li class="col-md-3 col-sm-6 col-xs-12 isotope-item instagram"><strong>From API Instagram</strong><div class="image-gallery-item"><div class="image-gallery-item"><span class="thumb-info thumb-info-hide-wrapper-bg"><span class="thumb-info-wrapper"><a href="{{link}}" target="_new" class="lightbox-portfolio"><img src="{{image}}" class="img-responsive" alt="" /></a><span class="thumb-info-caption"><span class="thumb-info-caption-text">{{caption}}</span></span></span></span></div></div></li>'
+		template: '<li class="col-md-3 col-sm-6 col-xs-12 isotope-item instagram"><strong>From API Instagram</strong><div class="image-gallery-item"><div class="image-gallery-item"><a href="{{link}}" target="_new" ><span class="thumb-info thumb-info-centered-info"><span class="thumb-info-wrapper"><img src="{{image}}" class="img-responsive" alt="" /><span class="thumb-info-title"><span class="thumb-info-type">Click For Details</span></span></span></span></a></div></div></li>'
 	});
 	feed.run();
 });
