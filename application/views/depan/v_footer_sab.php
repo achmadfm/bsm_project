4346f77f16dd84dbd1151f54f45096aeaab2b8dc
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2020-01-09 15:12:46 +0800

    tambah download brosur

--- application/views/depan/v_footer_sab.php
+++ application/views/depan/v_footer_sab.php
@@ -6,7 +6,6 @@
 			</div>
 			<div class="col-lg-3">
 				<div class="contact-details">
-				    <br>
     			        <h4><strong>PG - TK - SD</strong></h4>
     					<a target="_blank" href="https://www.google.com/maps/place/Sekolah+Alam+Bosowa/@-5.1701387,119.3943366,17z/data=!4m12!1m6!3m5!1s0x2dbee2a7d3bd8a69:0xe6fbf6d9f74bcd7a!2sSekolah+Alam+Bosowa!8m2!3d-5.1701387!4d119.3965253!3m4!1s0x2dbee2a7d3bd8a69:0xe6fbf6d9f74bcd7a!8m2!3d-5.1701387!4d119.3965253"><p><i class="fa fa-map-marker"></i> <strong></strong> Jl. Danau Tj. Bunga, Maccini Sombala, Tamalate, Makassar - 90224</p></a>
     			        
@@ -16,8 +15,10 @@
 					    
 				</div>
 			</div>
-			<div class="col-sm-1">
-			    
+			<div class="col-sm-3">
+				<h4><strong>Download Brosur</strong></h4>
+					<a href="#" data-toggle="modal" data-target="#defaultModal"><img src="<?php echo base_url('template/bsmd.png')?>" width="180px" height="80px" alt="download"></a><br>
+					<a href="#" data-toggle="modal" data-target="#defaultModal2"><img src="<?php echo base_url('template/sabd.png')?>" width="180px" height="80px" alt="download"></a>
 			</div>
 			<div class="col-lg-3">
 				<h5 class="mb-2">Call Us / Contact Persons</h5>
@@ -27,7 +28,7 @@
 					<li><i class="fa fa-envelope"></i> <a href="#">info@bosowaschool.sch.id</a></li>
 				</ul>
 			</div>
-			<div class="col-lg-2">
+			<div class="col-lg-3">
 				<h4>Follow Us</h4>
 				<ul class="social-icons">
 					<li class="social-icons-facebook"><a href="https://www.facebook.com/bosowa.bsm" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
@@ -47,3 +48,106 @@
 		</div>
 	</div>
 </footer>
+
+<!-- Modals SMP-SMA -->
+<div class="modal fade" id="defaultModal" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="defaultModalLabel">Download Brosur</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>
+							#
+						</th>
+						<th>
+							Unit
+						</th>
+						<th>
+							Files
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="active">
+						<th scope="row">
+							1
+						</th>
+						<td>
+							Brosur SMA Bosowa School Makassar
+						</td>
+						<td>
+							<a class="btn btn-sm btn-primary" href="<?php echo base_url('template/brosur/brosur_SMA.pdf')?>" download><i class="fa fa-download"></i> Download File</a>
+						</td>
+					</tr>
+					<tr class="info">
+						<th scope="row">
+							2
+						</th>
+						<td>
+							Brosur SMP Bosowa School Makassar
+						</td>
+						<td>
+						<a class="btn btn-sm btn-default" href="<?php echo base_url('template/brosur/brosur_SMP.pdf')?>" download><i class="fa fa-download"></i> Download File</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END OF MODALS SMP-SMA -->
+
+<!-- Modals TK-PG-SD -->
+<div class="modal fade" id="defaultModal2" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="defaultModalLabel">Download Brosur</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>
+							#
+						</th>
+						<th>
+							Unit
+						</th>
+						<th>
+							Files
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="success">
+						<th scope="row">
+							1
+						</th>
+						<td>
+							Brosur Baby House-PG-TK-SD Sekolah Alam Bosowa
+						</td>
+						<td>
+							<a class="btn btn-sm btn-default" href="<?php echo base_url('template/brosur/brosur_SAB.pdf')?>" download><i class="fa fa-download"></i> Download File</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END OF MODALS TK-PG-SD -->
