1e24af78fbb043611c88815ffa0e0e94c99a3010
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2020-01-13 11:06:22 +0800

    sedikit

--- template/js/instagram/sabhome.js
+++ template/js/instagram/sabhome.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
 		userId: '5585679322',
 		accessToken: '5585679322.1be78b3.0a7ccf4dcec54668b951b97783f9e75a',
 		target: 'instafeedsabhome',
+		limit: 9,
 		sortBy: 'most-recent',
-		limit:9,
 		resolution: 'thumbnail',
-		template:'<li><a href="{{link}}" target="_new"><span class="img-thumbnail d-block"><img class="img-fluid" width="70" height="70" src="{{image}}"></span></a><br><p><i class="fa fa-heart"></i> {{likes}}  <i class="fa fa-comment"></i> {{comments}}</p></li>',
+		template: '<li><a href="{{link}}" target="_new"><span class="img-thumbnail d-block"><img class="img-fluid" width="70" height="70" src="{{image}}"></span></a><br><p><i class="fa fa-heart"></i> {{likes}}  <i class="fa fa-comment"></i> {{comments}}</p></li>'
 	});
 	feed.run();
 });
